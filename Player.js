9ad5b3dcd1ffae9ef5bd25cebf1b7886ae211caa
SWAGERVIKING <83217822+Arjunbhaskhar07@users.noreply.github.com>
2021-05-13 17:03:17 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,53 @@
+class Player
+{
+    constructor() {
+		this.x = 500;
+		this.y=200;
+		this.spt=createSprite(this.x, this.y, 100,100);
+		this.spt.shapeColor="blue";
+
+	}
+
+  applyGravity()
+    {
+        this.spt.velocityY=this.spt.velocityY+2;
+	}
+	moveLeft()
+	{
+		this.spt.x=this.spt.x-12;
+	}
+	moveright()
+	{
+		this.spt.x=this.spt.x+12;
+	}
+	movejump()
+	{
+	this.spt.velocityY=-20
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
